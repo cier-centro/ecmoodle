c8daa2b78cccd1044bf43780e1b99b3ca736cb45
YesidDaza <u1801475@unimilitar.edu.co>
2017-11-08 12:16:26 -0500

    Changes in "Facilitadores" section

---
+++ conversatorio_facilitadores/index_edgeActions.js
@@ -0,0 +1,284 @@
+/***********************
+* Acciones de composición de Adobe Edge Animate
+*
+* Editar este archivo con precaución, teniendo cuidado de conservar 
+* las firmas de función y los comentarios que comienzan con "Edge" para mantener la 
+* capacidad de interactuar con estas acciones en Adobe Edge Animate
+*
+***********************/
+(function($, Edge, compId){
+var Composition = Edge.Composition, Symbol = Edge.Symbol; // los alias más comunes para las clases de Edge
+
+   //Edge symbol: 'stage'
+   (function(symbolName) {
+      
+      
+      Symbol.bindElementAction(compId, symbolName, "${_hotspot_1}", "click", function(sym, e) {
+         window.open("ec_rcf_1.html", "_self");
+         
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${_hotspot_2}", "click", function(sym, e) {
+         //window.open("ec_rcf_3.html", "_self");
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${_hotspot_3}", "click", function(sym, e) {
+         window.open("ec_rcf_3.html", "_self");
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${_hotspot_4}", "click", function(sym, e) {
+         window.open("ec_rcf_4.html", "_self");
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${_hotspot_5}", "click", function(sym, e) {
+         window.open("ec_rcf_5.html", "_self");
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${_hotspot_6}", "click", function(sym, e) {
+         window.open("ec_rcf_6.html", "_self");
+
+      });
+      //Edge binding end
+
+   })("stage");
+   //Edge symbol end:'stage'
+
+   //=========================================================
+   
+   //Edge symbol: 'etiqueta'
+   (function(symbolName) {   
+   
+   })("etiqueta");
+   //Edge symbol end:'etiqueta'
+
+   //=========================================================
+   
+   //Edge symbol: 'contenido'
+   (function(symbolName) {   
+   
+   })("contenido");
+   //Edge symbol end:'contenido'
+
+   //=========================================================
+   
+   //Edge symbol: 'btn_bienvenida'
+   (function(symbolName) {   
+   
+   })("btn_bienvenida");
+   //Edge symbol end:'btn_bienvenida'
+
+   //=========================================================
+   
+   //Edge symbol: 'btn_aprendizaje'
+   (function(symbolName) {   
+   
+   })("btn_aprendizaje");
+   //Edge symbol end:'btn_aprendizaje'
+
+   //=========================================================
+
+   //=========================================================
+   
+   //Edge symbol: 'brillo'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 2470, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("brillo");
+   //Edge symbol end:'brillo'
+
+   //=========================================================
+   
+   //Edge symbol: 'btillo_t'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 2000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("btillo_t");
+   //Edge symbol end:'btillo_t'
+
+   //=========================================================
+
+   //=========================================================
+
+   //=========================================================
+   
+   //Edge symbol: 'chest'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 25000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("chest");
+   //Edge symbol end:'chest'
+
+   //=========================================================
+   
+   //Edge symbol: 'trunk'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 23000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("trunk");
+   //Edge symbol end:'trunk'
+
+   //=========================================================
+   
+   //Edge symbol: 'head'
+   (function(symbolName) {   
+   
+   })("head");
+   //Edge symbol end:'head'
+
+   //=========================================================
+   
+   //Edge symbol: 'mouth'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 0, function(sym, e) {
+         sym.stop();
+
+      });
+      //Edge binding end
+
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 2626, function(sym, e) {
+         sym.play('speak');
+
+      });
+      //Edge binding end
+
+   })("mouth");
+   //Edge symbol end:'mouth'
+
+   //=========================================================
+   
+   //Edge symbol: 'character'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 18000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("character");
+   //Edge symbol end:'character'
+
+   //=========================================================
+   
+   //Edge symbol: 'eyes'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 23000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("eyes");
+   //Edge symbol end:'eyes'
+
+   //=========================================================
+   
+   //Edge symbol: 'character_1'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 21000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("character_1");
+   //Edge symbol end:'character_1'
+
+   //=========================================================
+   
+   //Edge symbol: 'ab_c'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 21000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("ab_c");
+   //Edge symbol end:'ab_c'
+
+   //=========================================================
+   
+   //Edge symbol: 'trunk_1'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 21000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("trunk_1");
+   //Edge symbol end:'trunk_1'
+
+   //=========================================================
+   
+   //Edge symbol: 'expressions'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 21000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("expressions");
+   //Edge symbol end:'expressions'
+
+   //=========================================================
+   
+   //Edge symbol: 'trunk_2'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 21000, function(sym, e) {
+         sym.play(0);
+
+      });
+      //Edge binding end
+
+   })("trunk_2");
+   //Edge symbol end:'trunk_2'
+
+   //=========================================================
+   
+   //Edge symbol: 'head_1'
+   (function(symbolName) {   
+   
+   })("head_1");
+   //Edge symbol end:'head_1'
+
+})(jQuery, AdobeEdge, "EDGE-217058342");
