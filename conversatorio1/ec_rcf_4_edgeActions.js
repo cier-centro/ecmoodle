c46f679544a61893eb2008ab7f43f23161a69522
Giovanni86 <giovanni.borda@gmail.com>
2017-11-14 20:21:41 -0500

    correción de la actividad

--- conversatorio1/ec_rcf_4_edgeActions.js
+++ conversatorio1/ec_rcf_4_edgeActions.js
@@ -187,7 +187,7 @@ var Composition = Edge.Composition, Symbol = Edge.Symbol; // los alias más comu
       //Edge binding end
 
       Symbol.bindElementAction(compId, symbolName, "${_hotspot_1}", "click", function(sym, e) {
-         //window.open("pdf/encuentro_virtual_2.pdf", "_blank");
+         window.open("http://application.colombiaaprende.edu.co/mod/scorm/view.php?id=286114", "_blank");
          sym.$("brillo_r1").hide();
 
       });
